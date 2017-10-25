50bbd596e740bdb3c8cc2cf2082fe3654b6b796b
Alexis Messier <alexis.messier@hotmail.fr>
2017-10-25 16:48:33 +0200

    feat commit

--- my-app/src/components/TestTable.js
+++ my-app/src/components/TestTable.js
@@ -3,33 +3,24 @@ import React from 'react'
 const PRODUCTS = [
   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'}
 ]
 
-class ProductRow extends React.Component {
+const testtablestyle = {
+  border: "5px solid purple",
+  margin: "100px",
+  padding: "50px"
+}
+
+class TestTable extends React.Component {
   render() {
     return (
-      <div>
-
+      <div
+        style={testtablestyle}>
+        test
       </div>
     )
   }
 }
 
-class ProductCategoryRow extends React.Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(
-  <FilterableProductTable products={PRODUCTS} />,
-  document.getElementById('container')
-)
+export default TestTable
